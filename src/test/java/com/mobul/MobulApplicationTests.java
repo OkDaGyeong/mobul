a9d9fc6057a60e391a35b1c2cc156380a3f79440
ParkH-Ram <parkeryangga@gmail.com>
2023-04-10 14:49:07 +0900

    23-4-10 Start project

---
+++ src/test/java/com/mobul/MobulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MobulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
